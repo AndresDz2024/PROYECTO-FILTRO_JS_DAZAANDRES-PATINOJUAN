49c1cf3e16b9b37b6098d03e3e521398db47de94
AndresDz2024 <daza45daza@gmail.com>
2024-05-13 14:24:58 -0500

    feat: :construction: Progreso en Rockets
    Se muestran a la perfección las imagenes de los cohetes y se muestra el bloque izquierdo

--- js/script.js
+++ js/script.js
@@ -50,13 +50,13 @@ function displayRocket(data) {
       <div class="caracC1">
         <h2>Atmospheric acceleration:</h2>
         <div>${calculo.toFixed(2)} %:</div>
-        <div>${valor.first_stage.thrust_sea_level.kN} kN <br> ${valor.first_stage.thrust_sea_level.lbf} lbf</div>
-      </div>
+        <div>${Intl.NumberFormat('cop').format(valor.first_stage.thrust_sea_level.kN)} kN <br> ${Intl.NumberFormat('cop').format(valor.first_stage.thrust_sea_level.lbf)} lbf</div>
+        </div>
 
       <div class="caracC2">
         <h2>Speed in space</h2>
         <div>${calculo2.toFixed(2)} %:</div>
-        <div>${valor.first_stage.thrust_vacuum.kN} kN <br> ${valor.first_stage.thrust_vacuum.lbf} lbf </div>
+        <div>${Intl.NumberFormat('cop').format(valor.first_stage.thrust_vacuum.kN)} kN <br> ${Intl.NumberFormat('cop').format(valor.first_stage.thrust_vacuum.lbf)} lbf</div>
       </div>
 
     </div>
@@ -86,20 +86,77 @@ function displayRocket(data) {
         <div>${valor.engines.propellant_1}</div>
         <div>Propellant 2:</div>
         <div>${valor.engines.propellant_2}</div>
+        <div>Thrust To Weight:</div>
+        <div>${valor.engines.thrust_to_weight}</div>
       </div>
     </div>
 
-    <div class="caracI">
-      <img class="imagen1" src="${valor.flickr_images[0]}">
+    <div class="caracL2">
+    <div class="InformationR"><h4>Information Rocket</h4></div>
+    <div class="caracL2-">
+      <div>Type:</div>
+      <div>${valor.type}</div>
+      <div>Active:</div>
+      <div>${valor.active}</div>
+      <div>Stages:</div>
+      <div>${valor.stages}</div>
+      <div>Boosters:</div>
+      <div>${valor.boosters}</div>
+      <div>Landing Legs:</div>
+      <div>${valor.landing_legs.number}</div>
+      <div>Leg Material:</div>
+      <div>${valor.landing_legs.material}</div>
+      <div>Payload Weights:</div>
+      <div>${valor.payload_weights.length}</div>
+      <div>Payload Weight 1 ID:</div>
+      <div>${valor.payload_weights[0].id}</div>
+      <div>Payload Weight 1 Name:</div>
+      <div>${valor.payload_weights[0].name}</div>
+      <div>Payload Weight 1 Kg:</div>
+      <div>${valor.payload_weights[0].kg} Kg</div>
+      <div>Payload Weight 1 Lb:</div>
+      <div>${valor.payload_weights[0].lb} Lb</div>
     </div>
-    <div class="caraL">
-      <div>${valor.description}</div> 
-      <div>${valor.first_flight}</div>
-      <div>${valor.company}</div>
-      <div>${valor.cost_per_launch}</div>
+  </div>
+
+
+    <div class="caracL">
+      <img src="../storage/img/mech.svg">
+      <div>
+        <h2>Description:</h2> 
+        <div>${valor.description}</div> 
+      </div>
+      <img src="../storage/img/mech.svg">
+      <div>
+        <h2>Country:</h2> 
+        <div>${valor.country}</div> 
+      </div>
+      <img src="../storage/img/mech.svg">
+      <div>
+        <h2>Company:</h2> 
+        <div>${valor.company}</div>
+      </div>
+      <img src="../storage/img/mech.svg">
+      <div>
+        <h2>The date of the first flight of the rocket:</h2> 
+        <div>${valor.first_flight}</div>
+      </div>
+      <img src="../storage/img/mech.svg">
+      <div>
+        <h2>The estimated cost per rocket launch:</h2> 
+        <div>$ ${Intl.NumberFormat('cop').format(valor.cost_per_launch)}</div>
+      </div>
+      <img src="../storage/img/mech.svg">
+      <div>
+        <h2>Read more about the Rocket:</h2> 
+        <a target="_blank" href="${valor.wikipedia}">Wikipedia</a>
+      </div>
     </div>
     `;
-}
+    if (valor.flickr_images.length > 0) {
+      displayImages(valor.flickr_images);
+    }
+  }
 
 function cambiarContador(valor) {
   contador = valor;
@@ -122,6 +179,16 @@ document.getElementById('button4').addEventListener('click', function() {
   cambiarContador(3);
 });
 
+function displayImages(images) {
+  const imageContainer = document.getElementById('imageContainer');
+  imageContainer.innerHTML = '';
 
+  images.forEach(image => {
+    const imgElement = document.createElement('img');
+    imgElement.src = image;
+    imgElement.classList.add('imagen1');
+    imageContainer.appendChild(imgElement);
+  });
+}
 
 fetchRocket();
